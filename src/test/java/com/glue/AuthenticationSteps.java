4a608b05d4231dcb7a491a364319390d11abfc2a
stephenpenn <34014345+stephenpenn@users.noreply.github.com>
2020-05-26 18:43:02 -0400

    This is my latest update to my scenario

--- src/test/java/com/glue/AuthenticationSteps.java
+++ src/test/java/com/glue/AuthenticationSteps.java
@@ -13,7 +13,12 @@ import org.junit.Assert;
 // Then something else should happen
 // And something else should happen
 // And I should see my account number
-
+// Scenario: NotImplementedPath
+//        Given I have opened the application
+//        When I put in valid credentials
+//        And click Login
+//        Then I should see my personalize home page
+//        And Something else happens
 public class AuthenticationSteps {
 
     @Given("^I have opened the application$")
@@ -38,6 +43,10 @@ public class AuthenticationSteps {
     
     @And("^I should see my account number$")
     public void i_should_see_my_account_number() throws Throwable {
+        Assert.assertEquals(true, true);
+    }
+    @And("^And Something else happens$")
+    public void and_something_else_happens() throws Throwable {
         Assert.assertEquals(true, true);
     }
 }
